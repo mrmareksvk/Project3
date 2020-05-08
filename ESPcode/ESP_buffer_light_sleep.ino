39be304fcc42609aeca61c2ff8697699f510fb37
Abrams-ZiZi <Abrams-ZiZi@users.noreply.github.com>
2020-05-08 16:16:21 +0200

    Removed unused variables, removed blank lines

--- ESPcode/ESP_buffer_light_sleep.ino
+++ ESPcode/ESP_buffer_light_sleep.ino
@@ -23,7 +23,7 @@
 #define RXPinGPS 4                                  // <-----
 #define TXPinGPS 5                                  // <-----
 #define DEEP_SLEEP 60e6                             // in microseconds
-#define MQTT_SERVER "broker.hivemq.com"             // <-----  LOCALHOST
+#define MQTT_SERVER "192.168.0.192"                 // <-----  LOCALHOST
 #define MQTT_PORT 1883                              // <-----  default
 #define ROOT_TOPIC "baaa"                           // Add '/' before device
 #define DEVICE_NAME "/sensor1"                      // <-----  original w. '/'
@@ -32,12 +32,11 @@
 float temperature, humidity;                        // raw dht data
 uint16 light;                                       // light val max 1024
 const uint16 light_treshold = 400;                  // minimal value to operate
-bool dataToSend = false;                            // flag for data sending
 bool maintenanceNeeded = false;                     // flag for maintenance
-bool maintenanceMode = false;                                           // mode flag
+bool maintenanceMode = false;                       // mode flag
 short sleepMode = 2;
 // TIMERS (49d. for millis roll-over)
-unsigned long lastRecon = 0, timer1 = 0, lastReconnect = 0, interval = 5000;
+unsigned long timer1 = 0, lastReconnect = 0, interval = 5000;
 // ARRAYS for MQTT values
 char temp_a[6], hum_a[4], light_a[5];               // char buffers for publish
 // -WORK-IN-PROGRESS
@@ -59,10 +58,10 @@ char lat_topic[20], lon_topic[20];
 char date_topic[20], time_topic[20];
 
 // MQTT ASSIGN PATH FUNCTION
-void pathAssign(void){
+void pathAssign(void) {
 // temperature
-	strcpy(temp_topic, ROOT_TOPIC);                             // Check readme.txt
-	strcat(temp_topic, DEVICE_NAME);                            // Check readme.txt
+	strcpy(temp_topic, ROOT_TOPIC);                  // Check readme.txt
+	strcat(temp_topic, DEVICE_NAME);                 // Check readme.txt
 	strcat(temp_topic, "/temp");
 // humidity
 	strcpy(hum_topic, ROOT_TOPIC);
@@ -105,8 +104,8 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 
 /*Put your SSID & Password*/
-const char* ssid = SECRET_SSID;                                                 // Secret.h
-const char* password = SECRET_PSW;                                              // Secret.h
+const char* ssid = SECRET_SSID;                         // Secret.h
+const char* password = SECRET_PSW;                      // Secret.h
 
 // MQTT SETUP
 PubSubClient mqttClient(espClient);
@@ -115,56 +114,40 @@ const char* mqtt_user = SECRET_MQTT_USER;
 const char* mqtt_psw = SECRET_MQTT_PSW;
 
 // FUNCTIONS
-bool measureTemp(void) {                                // <---------
+bool measureTemp(void) {
 	temperature = dht.readTemperature();
-	/*
-	   if isnan read last from buffer and copy it
-	 */
-	if(isnan(temperature)) {
-		Serial.println("maintenanceNeeded = true");
-		maintenanceNeeded = true;
+	// if isnan read last from buffer and copy it
+	if (isnan(temperature)) {
 		temperature = temp_stor.last();
 		temp_stor.push(temperature);
 		return false;
-	}else{
-		maintenanceNeeded = false;
+	} else {
 		temp_stor.push(temperature);
 		return true;
 	}
-
-
 }
 
-bool measureHum(void) {                                 // <----------
+bool measureHum(void) {
 	humidity = dht.readHumidity();
-	/*
-	   if isnan read last from buffer and copy it
-	 */
-	if(isnan(humidity)) {
-		maintenanceNeeded = true;
+	// if isnan read last from buffer and copy it
+	if (isnan(humidity)) {
 		humidity = hum_stor.last();
 		hum_stor.push(humidity);
 		return false;
-	}else{
-		maintenanceNeeded = false;
+	} else {
 		hum_stor.push(humidity);
 		return true;
 	}
-
 }
 
-bool measureLight(void) {                               // <----------
+bool measureLight(void) {
 	light = analogRead(LDR_PIN);
-	/*
-	   if isnan or not in interval 0 - 1024 read last from buffer and copy it
-	 */
-	if(isnan(light) || !(0 <= light && light <= 1024)) {
-		maintenanceNeeded = true;
+	  // if isnan or not in interval 0 - 1024 read last from buffer and copy it
+	if (isnan(light) || !(0 <= light && light <= 1024)) {
 		light = light_stor.last();
 		light_stor.push(light);
 		return false;
-	}else{
-		// maintenanceNeeded = false;
+	} else {
 		light_stor.push(light);
 		return true;
 	}
@@ -173,29 +156,22 @@ bool measureLight(void) {                               // <----------
 void gpsParser(void) {
 	if (ss.available() > 0) {
 		gps.encode(ss.read());
-
 		if (gps.location.isUpdated()) {
 			latitude = gps.location.lat();
 			longitude = gps.location.lng();
-
 		}
-
 		if (gps.date.isUpdated()) {
 			date_raw = gps.date.value();
-
 		}
-
 		if (gps.time.isUpdated()) {
 			time_raw = gps.time.value();
-
 		}
-
 	}
 }
 
 unsigned long blink = 0;
-void blinker(void){
-	if(millis()>=blink+1000) {
+void blinker(void) {
+	if (millis() >= blink + 1000) {
 		digitalWrite(IN_LED, !digitalRead(IN_LED));
 		blink = millis();
 	}
@@ -206,24 +182,20 @@ boolean reconnect(void) {
 	const char* clientId = DEVICE_NAME;
 	if (mqttClient.connect(clientId, mqtt_user, mqtt_psw)) {
 		sensorACK();
-		if(dataToSend) {
-			mqttPublish();
-		}
 	}
 	return mqttClient.connected();
 }
 
 void mqttPublish(void) {
 	while (!light_stor.isEmpty()) {
-
-		dtostrf(temp_stor.shift(), 4, 1, temp_a);                 // Check readme.txt
+		dtostrf(temp_stor.shift(), 4, 1, temp_a);            // Check readme.txt
 		dtostrf(hum_stor.shift(), 3, 0, hum_a);
-		sprintf(light_a, "%d", light_stor.shift());                                             // Check readme.txt
+		sprintf(light_a, "%d", light_stor.shift());			 // Check readme.txt
 		dtostrf(lat_stor.shift(), 9, 6, lat_a);
 		dtostrf(lon_stor.shift(), 10, 6, lon_a);
-		sprintf(date_a, "%d", date_stor.shift());                    // DDMMYY
-		sprintf(time_a, "%d", time_stor.shift());                    // HHMMSSCC
-		time_a[strlen(time_a)-2] = '\0';                    // Removes centy-seconds
+		sprintf(date_a, "%d", date_stor.shift());            // DDMMYY
+		sprintf(time_a, "%d", time_stor.shift());            // HHMMSSCC
+		time_a[strlen(time_a)-2] = '\0';                     // Removes centi-seconds
 
 		mqttClient.publish(temp_topic, temp_a);
 		mqttClient.publish(hum_topic, hum_a);
@@ -233,38 +205,31 @@ void mqttPublish(void) {
 		mqttClient.publish(date_topic, date_a);
 		mqttClient.publish(time_topic, time_a);
 
-		dataToSend = false;
-
-		delay(100); // Adam: added delay in order to not spam Python
+		delay(100); // Delay in order to not spam Python
 	}
 }
 
-void sensorACK(void){                                                                   // ACK to ROOT_TOPIC
+void sensorACK(void) {                                       // ACK to ROOT_TOPIC
 	char ack_msg[20];
-	if(maintenanceNeeded) {
+	if (maintenanceNeeded) {
 		strcpy(ack_msg, "maintenance");
 		strcat(ack_msg, DEVICE_NAME);
 		mqttClient.publish(ROOT_TOPIC, ack_msg);
-	}else{
+	} else {
 		strcpy(ack_msg, "working");
 		strcat(ack_msg, DEVICE_NAME);
 		mqttClient.publish(ROOT_TOPIC, ack_msg);
 	}
 }
 
-void dataStorage(void){                                 // <----------!!!
-
-}
-
-void sleepDeep(void){                                                                   // Erase RAM
-	system_deep_sleep_set_option(2);                        // Check readme.txt
+void sleepDeep(void) {                                       // Erases RAM
+	system_deep_sleep_set_option(2);                         // Check readme.txt
 	system_deep_sleep(DEEP_SLEEP);
 }
 
-void lightSleep(void){
+void sleepLight(void) {                                      // Keeps RAM
 	digitalWrite(IN_LED, HIGH);
-	delay(DEEP_SLEEP/1000);
-	// change micro to milli
+	delay(DEEP_SLEEP / 1000);								 // change micro to milli
 	measureLight();
 }
 
@@ -276,7 +241,7 @@ void sleep(void) {
 		sleepMode = 2;
 	}
 	if (sleepMode == 1) {
-		lightSleep();
+		sleepLight();
 	}
 	else if (sleepMode == 2) {
 		sleepDeep();
@@ -294,19 +259,17 @@ void setup() {
 		sleep();
 	} else {
 		// Check readme.txt
-		WiFi.disconnect();                              // Clears WiFi cache
+		WiFi.disconnect();                                 // Clears WiFi cache
 
 		// INIT
-		pathAssign();                                           // Init topic paths
-		//Serial.begin(9600);                           // Only for DEBUG
-		ss.begin(9600);                                         // GPS baudrate
+		pathAssign();                                      // Init topic paths
+		ss.begin(9600);                                    // GPS baudrate
 
 		dht.begin();
 
 		// WiFi SETUP
-		WiFi.mode(WIFI_STA);                                    // WiFi as station (NOT AP)
-//		WiFi.setSleepMode(WIFI_NONE_SLEEP);             // Full power, prevents random disconnects
-		wifi_set_sleep_type(LIGHT_SLEEP_T);
+		WiFi.mode(WIFI_STA);                               // WiFi as station (NOT AP)
+		wifi_set_sleep_type(LIGHT_SLEEP_T);                // allowing entry to light sleep during idle
 
 		WiFi.begin(ssid, password);
 		wifi_station_set_auto_connect(true);
@@ -316,73 +279,64 @@ void setup() {
 			digitalWrite(IN_LED, !digitalRead(IN_LED));
 			connectionAttemps++;
 			if (connectionAttemps >= 20) {
-				break;                                                          // Continue without network
+				break;
 			}
 		}
 
-		digitalWrite(IN_LED, HIGH);                             // Anti-logic
+		digitalWrite(IN_LED, HIGH);                        // reversed (HIGH == led off)
 
 		mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
 
-		// data filler and maintenance invoker
-		if (measureTemp() == false) {maintenanceNeeded=true;}
-		if (measureHum() == false) {maintenanceNeeded=true;}
-		if (measureLight() == false) {maintenanceNeeded=true;}
+		// first measurement and fail-check
+		if (measureTemp() == false) maintenanceNeeded = true;
+		if (measureHum() == false) maintenanceNeeded = true;
+		if (measureLight() == false) maintenanceNeeded = true;
 
 	}
 }
 
 void loop() {
-	if(light >= light_treshold && maintenanceMode == false) {
+	if (light >= light_treshold && maintenanceMode == false) {
 		// M Q T T  H A N D L E R
 		if (!mqttClient.connected()) {                          // Loss of connection
-			if (millis() >= lastReconnect + 2000) {               // reconnectInterval
+			if (millis() >= lastReconnect + 2000) {             // reconnectInterval
 				lastReconnect = millis();
-				if (reconnect()) {                                              // Try reconnect
-					lastReconnect = 0;                                                      // Succesfull reconection
+				if (reconnect()) {                              // Try reconnect
+					lastReconnect = 0;                          // Succesfull reconection
 				}
 			}
-		} else {                                                    // Connected-keep MQTT alive
+		} else {                                                // Connected-keep MQTT alive
 			mqttClient.loop();
 		}
 
 		// C O D E  P A R T
-		if(WiFi.status() == WL_CONNECTED && mqttClient.connected() && maintenanceNeeded) {
+		if (WiFi.status() == WL_CONNECTED && mqttClient.connected() && maintenanceNeeded) {
 			maintenanceMode = true;
 		}
 
 		// M E A S U R E M E N T S  5  M i n (TESTING SET FOR 5 SEC)
 		if (millis() >= timer1 + interval) {
-			measureTemp();
-			measureHum();
-			measureLight();
+			if (measureTemp() == false) maintenanceNeeded = true;
+			if (measureHum() == false) maintenanceNeeded = true;
+			if (measureLight() == false) maintenanceNeeded = true;
 
 			lat_stor.push(latitude);
 			lon_stor.push(longitude);
 			date_stor.push(date_raw);
 			time_stor.push(time_raw);
 
-			if(WiFi.status() == WL_CONNECTED && mqttClient.connected()) {
+			if (WiFi.status() == WL_CONNECTED && mqttClient.connected()) {
 				mqttPublish();
-			}else{
-
-				// dataStorage();                           // <----- ! ! !
-				delay(10);
 			}
 			timer1 = millis();
 		}
-
 		gpsParser();                                                // Funnel data to GPS obj
 		blinker();                                                  // Debug
 
-	}else{
+	} else {
 		sensorACK();
 		mqttClient.publish(ROOT_TOPIC, "sleep we go");
 		delay(100);
-		/*If arrays are empty (or only 1) deepSleep*/
-		// deepSleep();
 		sleep();
-
 	}
-
 }
